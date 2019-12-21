4d88b792b4132d0a39be0ebc7cd62b29b63b7265
Bartosz Gaj <bartosz.gaj4@gmail.com>
2019-12-21 21:57:44 +0100

    backend server

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,50 @@
 const express = require('express');
 const app = express();
+const expressWs = require('express-ws')(app);
+const bodyParser = require("body-parser");
 
-app.get('/', function (req, res) {
- return res.send('Hello world');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+let sockets = [];
+let recognitions = {};
+
+// GET endpoint for getting simple recognitions data
+app.get('/recognition', function (req, res) {
+    return res.send(recognitions);
 });
 
-app.listen(process.env.PORT || 8080);
+// POST endpoint for sending recognitions data from python script
+app.post('/recognition', function (req, res) {
+    recognitions = req.body;
+    try {
+        // send updated data to every established websocket
+        sockets.forEach(function (socket) {
+            socket.send(JSON.stringify(recognitions))
+        })
+    } catch (e) {
+        console.log(e.message)
+        return res.send("false");
+    }
+    return res.send("true");
+});
+
+// web socket endpoint
+app.ws('/ws', function (ws, req) {
+
+    // connection has opened. Add socket to sockets list
+    sockets.push(ws);
+    console.log("Added new socket");
+
+    // connection has closed. Delete socket from sockets list
+    ws.on('close', function (msg) {
+        let index = sockets.indexOf(ws);
+        sockets.splice(index, 1);
+        console.log("Deleted disconnected socket");
+    })
+});
+
+// start server on port 8080
+app.listen(8080, function () {
+    console.log('Smart mirror server listening on port 8080!')
+});
